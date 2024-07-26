b14cc9a54dc4647e7b813446e33abd16d3332e37
Andreas Pabst <kontakt@andreas-pabst.de>
2024-07-26 15:21:42 +0200

    initial commit of index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Import necessary modules from vue
+import type { App, Component } from 'vue';
+
+/**
+ * Interface for Vue3Head
+ * @interface Vue3Head
+ */
+interface Vue3Head {
+    // Method to install the plugin
+    install: (app: App) => void;
+}
+
+export class createVue3Head {
+    static install(app: App) {
+
+    }
+}
